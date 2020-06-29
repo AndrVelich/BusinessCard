7f3f22882f8fe1041f57dc6456a3a506e787c6e7
Andrei Velichko <andr.velich@gmail.com>
2020-06-29 15:58:35 +0300

    initial

---
+++ BusinessCard/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("~/dist/index.html", "text/html");
+        }
+    }
+}
